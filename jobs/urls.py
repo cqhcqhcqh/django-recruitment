253363dd1a62a34802b91e747ca8bbfadc0004bb
caitou <qihan.cheng@versa-ai.com>
2023-07-19 17:08:40 +0800

    1. 浏览职位列表 2. 职业详情页 - views 里面定义接口函数<从数据库中获取职位 Job>    - 使用模板语法填充构建 html 内容    - 指定 url 使用哪个模板 - urls 里面定义 url 跳转规则 - 将应用(jobs)中的跳转规则配置到项目的 urls.py 中

---
+++ jobs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from jobs import views
+
+urlpatterns = [
+    re_path(r"^joblist/", views.joblist, name="joblist"),
+    # \d+ 匹配数字
+    # /$ 以 `/`结尾
+    # (?P<job_id>\d+) 中的 (?P<job_id> 部分是一个命名捕获组，用于在 URL 模式中捕获特定的值，并将其传递给视图函数作为参数
+    # 给 job(request, job_id) 函数来获取 render 的结果
+    re_path(r"^job/(?P<job_id>\d+)/$", views.job, name="job")
+]
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from jobs.models import Job
+from jobs.models import Job_citys, Job_types
+
+from django.http import Http404
+
+# Django 视图加载有好几种方法
+# 1. 利用函数来定义
+# 2. 利用视图的类来定义
+def joblist(request):
+    job_list = Job.objects.order_by('job_type')
+
+    # job_list 拿到的应该不是 Job 对象数组？
+    # 还说可以给 Job 对象动态添加 type_name 和 city_name 属性？
+    # type_name 和 city_name 显然不是 Job 的属性
+    for job in job_list:
+        job.type_name = Job_types[job.job_type][1]
+        job.city_name = Job_citys[job.job_city][1]
+
+    template = loader.get_template('joblist.html')
+    context = {'job_list': job_list}
+    return HttpResponse(template.render(context))
+
+def job(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        job.type_name = Job_types[job.job_type][1]
+        job.city_name = Job_citys[job.job_city][1]
+
+        # 1
+        # context = {'job': job}
+        # template = loader.get_template('job.html')
+        # return HttpResponse(template.render(context))
+
+        # 2
+        return render(request, 'job.html', {'job': job})
+    except Job.DoesNotExist:
+        raise Http404('Job does not exist')
